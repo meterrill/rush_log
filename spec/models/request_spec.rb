873159a25d5d6bfd158506271e1d9294b58fb2da
Matthew Terrill <meterrill@gmail.com>
2015-06-24 18:44:36 +0000

    Add spec for presence of borrower name and make it pass

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Request do
   it { should validate_presence_of :loan_number }
   it { should validate_length_of :loan_number }
+  it { should validate_presence_of :borrower_name }
 end
