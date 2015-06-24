7af1f34ff29aeea979c376432c0511a2bfdc1976
Matthew Terrill <meterrill@gmail.com>
2015-06-24 18:45:07 +0000

    Add spec for presence of reason and make it pass

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,4 @@
 class Request < ActiveRecord::Base
-  validates :loan_number, :borrower_name, :presence => true
+  validates :loan_number, :borrower_name, :reason, :presence => true
   validates :loan_number, :length => { :is => 11 }
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -4,4 +4,5 @@ describe Request do
   it { should validate_presence_of :loan_number }
   it { should validate_length_of :loan_number }
   it { should validate_presence_of :borrower_name }
+  it { should validate_presence_of :reason }
 end
