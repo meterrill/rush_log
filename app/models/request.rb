0485dff3777c890e6e02f161c02ae1635266c459
Matthew Terrill <meterrill@gmail.com>
2015-06-24 18:43:37 +0000

    Add spec for length of loan number and make it pass

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,4 @@
 class Request < ActiveRecord::Base
   validates :loan_number, :presence => true
+  validates :loan_number, :length => { :is => 11 }
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Request do
   it { should validate_presence_of :loan_number }
+  it { should validate_length_of :loan_number }
 end
