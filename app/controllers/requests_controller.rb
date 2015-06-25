14a699e65a8632ff2793f38122161a7bde5e30db
Matthew Terrill <meterrill@gmail.com>
2015-06-25 18:00:17 +0000

    Add edit/update actions/view

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -20,6 +20,19 @@ class RequestsController < ApplicationController
     @request = Request.find(params[:id])
   end
 
+  def edit
+    @request = Request.find(params[:id])
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    if@request.update(request_params)
+      render :show
+    else
+      render :edit
+    end
+  end
+
 private
   def request_params
     params.require(:request).permit(:loan_number,
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resource :requests
+  resources :requests
 end
