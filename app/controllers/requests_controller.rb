65c35595b41a33880ac9d428056b52f4b6d812be
Matthew Terrill <meterrill@gmail.com>
2015-06-25 17:25:36 +0000

    Add new/create actions/views

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,21 @@
 class RequestsController < ApplicationController
   def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+    if@request.save
+      redirect_to requests_path
+    else
+      render :new
+    end
+  end
+
+private
+  def request_params
+    params.require(:request).permit(:loan_number,
+                                    :borrower_name,
+                                    :reason)
   end
 end
