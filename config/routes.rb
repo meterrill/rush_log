5b4abdc65b61838171362f3b2d742c555b5ca257
Matthew Terrill <meterrill@gmail.com>
2015-07-01 21:58:59 +0000

    Update path verbiage for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, path_names: {sign_in: "login", sign_out: "logout"}
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   root to: 'requests#index'
 
