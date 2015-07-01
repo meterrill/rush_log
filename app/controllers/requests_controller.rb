72c1c8b95126c57b13fa8007c84d867a27a0a8db
Matthew Terrill <meterrill@gmail.com>
2015-07-01 22:02:27 +0000

    Update before filter for requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,5 @@
 class RequestsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_admin! || :authenticate_user!
 
   helper_method :sort_column, :sort_direction
 
