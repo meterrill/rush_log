5db645928cae1f1a042688d4d2fe97336d8d34b9
Matthew Terrill <meterrill@gmail.com>
2015-07-02 23:31:02 +0000

    Add sortable table columns for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
 class UsersController < ApplicationController
   before_filter :require_admin
 
+  helper_method :sort_column, :sort_direction
+
   def index
-    @users = User.all
+    @users = User.order(sort_column + " " + sort_direction)
   end
 
 private
   def require_admin
     unless admin_signed_in?
     flash[:error] = "You must be logged in as admin to access this section."
-    redirect_to root_path
+    redirect_to :back
     end
+  end
+
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "email"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
