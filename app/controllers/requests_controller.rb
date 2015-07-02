26c76caa25835559c6e34b207d1fd0fe329dc2a1
Matthew Terrill <meterrill@gmail.com>
2015-07-02 21:57:36 +0000

    Update require login method for requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -48,9 +48,8 @@ class RequestsController < ApplicationController
 
 private
   def require_login
-    if
-      user_signed_in? || admin_signed_in?
-    else
+    unless user_signed_in? || admin_signed_in?
+      flash[:error] = "You must be logged in to access this section."
       redirect_to new_user_session_path
     end
   end
